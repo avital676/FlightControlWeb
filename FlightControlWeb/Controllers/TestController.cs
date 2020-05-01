13b318628cbcd75d001c6929b25e18f63ce66811
avital levita <avital.levita@live.biu.ac.il>
2020-05-01 19:04:50 +0300

    controler test

---
+++ FlightControlWeb/Controllers/TestController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace FlightControlWeb.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        private readonly ILogger<TestController> _logger;
+
+        public TestController(ILogger<TestController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+          //  StreamReader reader = new StreamReader(Server.MapPath("~/testhtml.htm"));
+          //  string readFile = reader.ReadToEnd();
+            return "This is a test";
+        }
+
+        public List<string> Get(int Id)
+        {
+            return new List<string> {
+                "Data1",
+                "Data2"
+            };
+        }
+    }
+}
---
+++ FlightControlWeb/Test.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlightControlWeb
+{
+    public class Test
+    {
+
+        public Test()
+        {
+            
+            string curDir = Directory.GetCurrentDirectory();
+        //    this.webBrowser1.Url = new Uri(String.Format("file:///{0}/htmlpage.html", curDir));
+        }
+        
+
+    }
+}
