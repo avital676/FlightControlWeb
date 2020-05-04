af35ac416ebd3e3a6e4397a5aeea9332c8420a3d
avital levita <avital.levita@live.biu.ac.il>
2020-05-04 21:51:47 +0300

    db

--- FlightControlWeb/DataBase/Segment.cs
+++ FlightControlWeb/DataBase/Segment.cs
@@ -5,13 +5,10 @@ using System.Threading.Tasks;
 
 namespace FlightControlWeb.DataBase
 {
-    public class FlightPlan
+    public class Segment
     {
-        public int passengers { get; set; }
-        public string company_name { get; set; }
         public double longitude { get; set; }
         public double latitude { get; set; }
-        public string date_time { get; set; }
-        public List<Segment> segments { get; set; }
+        public double timespan_seconds { get; set; }
     }
 }
