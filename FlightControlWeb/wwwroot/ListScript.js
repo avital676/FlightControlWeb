d0a8de96171f8b109eef89793d07d29585bc748c
avital levita <avital.levita@live.biu.ac.il>
2020-05-05 17:28:46 +0300

    list script

---
+++ FlightControlWeb/wwwroot/ListScript.js
@@ -0,0 +1,14 @@
+﻿function allowDrop(ev) {
+    $("#listsArea").hide();
+    $("#dragAndDrop").show();
+    ev.preventDefault();
+    event.dataTransfer.setData("text/plain", event.target.id)
+    document.getElementById("detailes").innerHTML = "drag";
+}
+
+function onDrop(ev) {
+    document.getElementById("detailes").innerHTML = "drropppopopo";
+    document.getElementById("dragAndDrop").style.display = "none"
+    $("#listsArea").show();
+    ev.preventDefault();
+}
