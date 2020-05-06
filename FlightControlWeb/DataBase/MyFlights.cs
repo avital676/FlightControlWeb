f3560b1e7b51b937e3fbb564fcc8b1f62dfc5feb
green <green@LAPTOP-SR93CP99>
2020-05-06 08:53:42 +0300

    little change

--- FlightControlWeb/DataBase/MyFlights.cs
+++ FlightControlWeb/DataBase/MyFlights.cs
@@ -9,7 +9,9 @@ namespace FlightControlWeb.DataBase
     {
         private static List<Flight> myFlights = new List<Flight>()
         {
-             new Flight{ flight_id="1234",longitude= 98,latitude = 0,passengers= 90, company_name ="elal", date_time="04.05.20",is_external = true } 
+             new Flight{ flight_id="1234",longitude= 98,latitude = 0,passengers= 90, company_name ="elal", date_time="04.05.20",is_external = true },
+             new Flight{ flight_id="5678",longitude= 98,latitude = 0,passengers= 100, company_name ="arkia", date_time="09.10.20",is_external = true },
+            new Flight{ flight_id="9012",longitude= 98,latitude = 0,passengers= 80, company_name ="Tailand", date_time="18.09.20",is_external = true } 
         };
     
         public IEnumerable<Flight> getAllFlights(){
