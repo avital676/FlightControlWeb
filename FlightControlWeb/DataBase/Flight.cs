dc5f81dd4ebfc2671927059374d903620dc87884
shaiac <58950699+shaiac@users.noreply.github.com>
2020-05-08 11:28:00 +0200

    Update Flight.cs

--- FlightControlWeb/DataBase/Flight.cs
+++ FlightControlWeb/DataBase/Flight.cs
@@ -1,4 +1,4 @@
-﻿using FlightControlWeb.DataBase;
+using FlightControlWeb.DataBase;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +9,18 @@ using System.Globalization;
 
 namespace FlightControlWeb.Models
 {
+    public struct Info
+    {
+        public Info(int segmentNum, double secTillSegment)
+        {
+            SegmentNum = segmentNum;
+            SecTillSegment = secTillSegment;
+        }
+
+        public int SegmentNum { get; }
+        public double SecTillSegment { get; }
+    }
+
     public class Flight
     {
         public FlightPlan flightPlan { get; set; }
@@ -20,16 +32,21 @@ namespace FlightControlWeb.Models
             this.Latitude = flightPlan.Latitude;
             IsExternal = false;
             this.FlightId = CreateID();
-            this.DateTimee = DateTime.Now.ToString();
         }
         public string FlightId { get; set; }
         public double Longitude { get; set; }
         public double Latitude { get; set; }
-        public string DateTimee { get; set; }
         public bool IsExternal { get; set; }
         //public FlightPlan flightPlan { get; set; }
 
-
+        public string DateTimee
+        {
+            get { return flightPlan.DateTime; }
+            set
+            {
+                flightPlan.DateTime = value;
+            }
+        }
         public int Passengers
         {
             get { return flightPlan.Passengers; }
@@ -65,6 +82,43 @@ namespace FlightControlWeb.Models
         {
             return this.flightPlan;
         }
-    }
+        public string UpdateLocation(DateTime relativeTo)
+        {
+            Location startingLoc = new Location(0,0);
+            Location latLonToMove;
+            DateTime startingTime = DateTime.Parse(DateTimee);
+            double secGap = (relativeTo - startingTime).TotalSeconds;
+            Info currentSegment = getCurrentSegmentNum(relativeTo);
+            int currentSegmentNum = currentSegment.SegmentNum;
+            if (currentSegmentNum == -1) {
+                return "Ended";
+            } else if(currentSegmentNum == 0) {
+                startingLoc = new Location(flightPlan.Latitude, flightPlan.Longitude);
+            } else {
+                startingLoc = new Location(flightPlan.Segments[currentSegmentNum - 1].Latitude,
+                    flightPlan.Segments[currentSegmentNum - 1].Longitude);
+            }
+            latLonToMove = flightPlan.Segments[currentSegmentNum].MovementForSec(startingLoc.Lat, startingLoc.Lon);
+            Latitude = flightPlan.Segments[currentSegmentNum - 1].Latitude + (secGap - currentSegment.SecTillSegment) * latLonToMove.Lat;
+            Longitude = flightPlan.Segments[currentSegmentNum - 1].Longitude + (secGap - currentSegment.SecTillSegment) * latLonToMove.Lon;
+            return "Success";
+        }
 
+        private Info getCurrentSegmentNum(DateTime relativeTo)
+        {
+            double timePassed = 0;
+            int segmentNum = 0;
+            DateTime startingTime = DateTime.Parse(DateTimee);
+            double secGap = (relativeTo - startingTime).TotalSeconds;
+            for (segmentNum =0; segmentNum < flightPlan.Segments.Count; segmentNum++)
+            {
+                timePassed += flightPlan.Segments[segmentNum].TimeSpanSec;
+                if (secGap < timePassed)
+                {
+                    return new Info(segmentNum, timePassed);
+                }
+            }
+            return new Info(-1, timePassed); ;
+        }
+    }
 }
