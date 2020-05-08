36feceadb0afe42c780204541da1b447fa7ea0e9
shaiac <58950699+shaiac@users.noreply.github.com>
2020-05-08 11:30:18 +0200

    Update Segment.cs

--- FlightControlWeb/DataBase/Segment.cs
+++ FlightControlWeb/DataBase/Segment.cs
@@ -1,14 +1,39 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace FlightControlWeb.DataBase
 {
+    public struct Location
+    {
+        public Location(double lat, double lon)
+        {
+            Lat = lat;
+            Lon = lon;
+        }
+
+        public double Lat { get; }
+        public double Lon { get; }
+
+        public override string ToString() => $"({Lat}, {Lon})";
+    }
     public class Segment
     {
-        public double longitude { get; set; }
-        public double latitude { get; set; }
-        public double timespan_seconds { get; set; }
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
+        public double TimeSpanSec { get; set; }
+
+        public int SegmentPartsNum { get; set; }
+
+        public Location MovementForSec(double startLat, double startLon)
+        {
+            double latDistanceForSec = (Latitude - startLat) / TimeSpanSec;
+            double lonDistanceForSec = (Longitude - startLon) / TimeSpanSec;
+            Location loc = new Location(latDistanceForSec, lonDistanceForSec);
+            return loc;
+        }
     }
 }
+
+
